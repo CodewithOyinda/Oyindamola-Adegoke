6d2d24e7b8b7482a9d87d7e0f5d15c346ed0ba02
CodewithOyinda <honeybee08186@gmail.com>
2024-02-21 22:27:19 +0100

    Active tabs in progess

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 import { RiTwitterXFill } from "react-icons/ri";
 import { AiFillInstagram } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className='h-[100px]'>
        <div className='flex justify-center items-center gap-[2rem] mt-[60px] mb-[20px]'>
-       <IoLogoLinkedin />
-       <RiTwitterXFill />
-       <IoLogoGithub />
-       <AiFillInstagram />
+       <Link to='http://linkedin.com/in/oyindamola-adegoke'><IoLogoLinkedin /></Link>
+       <Link to='https://twitter.com/AdegokeOyindam9'><RiTwitterXFill /></Link>
+       <Link to='https://github.com/CodewithOyinda'><IoLogoGithub /></Link>
+       <Link to='https://instagram.com/o.y.i.n.d.a_?igshid=YmMyMTA2M2Y='><AiFillInstagram /></Link>
        </div>
        <div className='flex justify-center items-center'>
         <p>Oyindamola © 2023</p>
--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -4,10 +4,15 @@ import oyinda from './assets/oyinda.png';
 import vector from './assets/Vector.svg'
 import { MdPermContactCalendar, MdShoppingBag } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 
 const LandingPage = () => {
   return (
     <div>
+        <Header />
         <div>
             <img src={background} alt='background' className='h-[416px] w-full'/>
         </div>
@@ -18,13 +23,15 @@ const LandingPage = () => {
             </div>
         </div>
         <div className='ml-[100px]'>
-            <p className='text-[56px] font-[700]'>Oyindamola Adegoke</p><img src={vector} alt='vector'/>
+            <div className='flex gap-[1rem] text-[56px] font-[700]'><p>Oyindamola Adegoke</p><img src={vector} alt='vector' className='w-[50px]'/></div>
             <p className='text-[24px] font-[400] pt-[20px]'>I build visually stunning and interactive websites, and I love what I do.</p>
             <div className='flex gap-[4rem] pt-[20px]'>
                 <p className='text-[24px] font-[400] text-[#7E7E7E] flex justify-center items-center gap-[1rem]'><MdShoppingBag /> Frontend Developer</p>
                 <p className='text-[24px] font-[400] text-[#7E7E7E] flex justify-center items-center gap-[1rem]'><FaLocationDot /> Abuja, Nigeria</p>
             </div>
         </div>
+        <Nav/>
+        <Footer/>
     </div>
   )
 }

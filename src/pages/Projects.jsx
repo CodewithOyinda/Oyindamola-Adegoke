b0a9283aba05f7f539925692ac9a4566c7c8b9a2
CodewithOyinda <honeybee08186@gmail.com>
2024-02-24 22:39:08 +0100

    Projects added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,108 @@
 import React from 'react';
+import avatar from './assets/Avatar.png';
+import { Link } from 'react-router-dom';
+import loanify from './assets/Loanify.png';
+import timesync from './assets/Timesync.png';
 
 
 const Projects = () => {
   return (
     <div className='mt-[50px] ml-[100px] mr-[100px]'>
-        <div className='flex'>
+        <div className='flex gap-[2rem]'>
            <div>
-            <img/>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
             </div>
             <div>
-                <p><span>Oyindamola .</span> 10 Oct 2022</p>
-                <p>Loanify App</p>
-                <p>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>TimeSync</p>
+                <p className='text-[20px]'>TimeSync is a web app that offers seemless experience in time tracking, ensuring precision and efficiency. Beyond that, TimeSync is your partner in effective project management, providing tools and features to enhance collaboration and productivity. </p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={timesync} alt='timesync' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div>
+        <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div>
+        <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div>
+        <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div>
+        <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div>
+        <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
+            </div>
+        </div> <div className='flex gap-[2rem] mt-[60px]'>
+           <div>
+            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            </div>
+            <div>
+                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2022</p>
+                <p className='text-[30px] font-[600] pb-[30px]'>Loanify App</p>
+                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <div>
+                    <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
+                </div>
             </div>
         </div>
     </div>

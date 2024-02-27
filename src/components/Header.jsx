a60c6c9f8cbb1a22242c8b27321a07fd87970f1c
CodewithOyinda <honeybee08186@gmail.com>
2024-02-27 22:46:28 +0100

    Responsiveness in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import vector from './assets/Vector.svg';
 
 const Header = () => {
   return (
-    <div className='flex ml-[50px] mr-[50px]'>
-        <div className='flex gap-[1rem] text-[24px]'>
-            <img src={vector} alt='vector' className='w-[50px]'/>
+    <div className='flex md:ml-[50px] ml-[20px] md:mr-[50px] mr-[20px]'>
+        <div className='flex gap-[1rem] md:text-[24px] text-[16px]'>
+            <img src={vector} alt='vector' className='md:w-[50px] w-[30px]'/>
             <p className=' font-squarePeg text-[30px]'>Oyindamola Adegoke</p>
         </div>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Feeds from '../pages/Feeds';
 import About from '../pages/About';
 import Projects from '../pages/Projects';
@@ -6,54 +6,54 @@ import Resume from '../pages/Resume';
 
 const Nav = () => {
     const [activeTab, setActiveTab] = useState("Feeds");
-   
+
     let menuTabs = [
         {
-          name: "Feeds",
-          fn: () => setActiveTab("Feeds"),
+            name: "Feeds",
+            fn: () => setActiveTab("Feeds"),
         },
         {
-          name: "About Me",
-          fn: () => setActiveTab("About Me"),
+            name: "About Me",
+            fn: () => setActiveTab("About Me"),
         },
         {
-          name: "My Projects",
-          fn: () => setActiveTab("My Projects"),
+            name: "My Projects",
+            fn: () => setActiveTab("My Projects"),
         },
         {
             name: "My Resume",
             fn: () => setActiveTab("My Resume"),
-          },
-      ];
+        },
+    ];
 
-  return (
-    <div>
-        <div className='flex justify-center items-center gap-[6rem] border-solid border-b-2 border-custom-[#c2c2c2] text-[22px] font-[600] mt-[40px] ml-[100px] mr-[100px]'>
-        {menuTabs.map((tab, index) => (
-                <button
-                  key={index}
-                  onClick={tab.fn}
-                  className={`${
-                    activeTab == tab.name && "w-[220px] h-[64px] text-[#1E1E1E] border-solid border-b-4 border-custom-[#fa2b66]"
-                  } p-[12px] flex rounded-[8px]`}
-                >
-                  {tab.name}
-                </button>
-              ))}
+    return (
+        <div className="w-full">
+            <div className='flex justify-center md:justify-start items-center gap-[1rem] border-solid border-b-2 border-custom-[#c2c2c2] md:text-[22px] text-[10px] font-[600] mt-[40px] md:ml-[100px] ml-[20px] md:mr-[100px] mr-[20px]'>
+                {menuTabs.map((tab, index) => (
+                    <button
+                        key={index}
+                        onClick={tab.fn}
+                        className={`${
+                            activeTab === tab.name ? "md:w-[220px] md:h-[64px] w-[80px] text-[#1E1E1E] border-solid border-b-4 border-custom-[#fa2b66] md:p-[12px] p-[2px] flex rounded-[8px]" : "md:w-[160px] md:h-[48px] w-[70px] text-[#1E1E1E] md:p-[10px] p-[2px] flex rounded-[8px]"
+                        }`}
+                    >
+                        {tab.name}
+                    </button>
+                ))}
             </div>
-            <div className="w-full min-h-[calc(100vh-176px)]">
-            {activeTab == "Feeds" ? (
-              <Feeds />
-            ) : activeTab == "About Me" ? (
-              <About />
-              ) : activeTab == "My Projects" ? (
-                <Projects />
-            ) : (
-              activeTab == "My Resume" && <Resume />
-            )}
-          </div>
-    </div>
-  )
+            <div>
+                {activeTab === "Feeds" ? (
+                    <Feeds />
+                ) : activeTab === "About Me" ? (
+                    <About />
+                ) : activeTab === "My Projects" ? (
+                    <Projects />
+                ) : (
+                    activeTab === "My Resume" && <Resume />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Nav

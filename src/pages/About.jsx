ebd77f7c22058b2434b7b94d6276dbc66fa8a0d5
CodewithOyinda <honeybee08186@gmail.com>
2024-02-29 23:10:17 +0100

    Feeds, about and project responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,42 +5,42 @@ import present from './assets/About.svg';
 const About = () => {
   return (
     <>
-    <div className='flex ml-[100px] mr-[100px] justify-between mt-[50px]'>
+    <div className='flex md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px] justify-between mt-[50px]'>
         <div className='h-[428px] w-[428px]'>
         <div className='flex gap-[1rem] items-center align-middle'>
-            <p className='text-[30px] font-[600] pb-[30px]'>Hello</p>
-            <img src={wave} alt='wave' className='w-[40px] mt-[-15px]'/>
+            <p className='md:text-[30px] text-[18px] font-[600] pb-[30px]'>Hello</p>
+            <img src={wave} alt='wave' className='md:w-[40px] w-[20px] md:mt-[-15px] mt-[-22px]'/>
         </div>
-        <div className='text-[22px]'>
+        <div className='md:text-[22px] text-[16px]'>
             <p>I am <span className='font-[600]'>Adegoke Deborah Oyindamola,</span> and my name carries significant meanings. <span className='font-[600]'>"Adegoke"</span> symbolizes <span className='font-[600]'>"The crown has been exalted"</span>, and <span className='font-[600]'>"Oyindamola"</span> represents <span className='font-[600]'>"Honey mixed with wealth".</span> While I am commonly known as <span className='font-[600]'>"Oyindamola",</span> I use <span className='font-[600]'>"Deborah"</span> professionally as it is my first name.</p>
         </div>
         </div>
         <div>
-            <img src={oyinda} alt='oyinda' className='w-[428px] h-[428px] rounded-[1rem]'/>
+            <img src={oyinda} alt='oyinda' className='md:w-[428px] md:h-[428px] w-[350px] rounded-[1rem]'/>
         </div>
     </div>
-    <div className='ml-[100px] mr-[100px] pt-[50px]'>
-        <p className='text-[25px] pb-[30px]'>Past</p>
-        <p className='text-[20px]'>I was born and brought up in Niger state, Nigeria. My childhood was filled with joyful moments of play, quality time with my family, and the regularity of school and church. The fond memories of those times are precious to me.
+    <div className='md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px] pt-[50px]'>
+        <p className='md:text-[25px] pb-[30px] text-[18px]'>Past</p>
+        <p className='md:text-[20px] text-[16px]'>I was born and brought up in Niger state, Nigeria. My childhood was filled with joyful moments of play, quality time with my family, and the regularity of school and church. The fond memories of those times are precious to me.
 As I grew up, I developed a strong interest in cooking, which eventually led me to pursue a degree in Food Science and Technology.
 It was during my time at university that I also discovered my talent for makeup artistry, and I realized my deep passion for enhancing the beauty of things.</p>
     </div>
-    <div className='ml-[100px] mr-[100px] pt-[50px]'>
-        <p className='text-[25px] pb-[30px]'>Present</p>
-        <p className='text-[20px]'>As you know, my passion for beautifying things has been a driving force in my life. While I thoroughly enjoyed my time as a makeup artist, I made the decision to explore another realm where I can channel my passion and creativity. I transitioned into the field of frontend development, and it has been an incredible experience so far.</p>
-        <p className='text-[20px]'>It allows me to utilize my eye for aesthetics, attention to detail, and understanding of color harmony in a digital context. Just as a makeup artist enhances natural beauty, I now have the ability to transform digital landscapes and create visually stunning user experiences.</p>
+    <div className='md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px] pt-[50px]'>
+        <p className='md:text-[25px] pb-[30px] text-[18px]'>Present</p>
+        <p className='md:text-[20px] text-[16px]'>As you know, my passion for beautifying things has been a driving force in my life. While I thoroughly enjoyed my time as a makeup artist, I made the decision to explore another realm where I can channel my passion and creativity. I transitioned into the field of frontend development, and it has been an incredible experience so far.</p>
+        <p className='md:text-[20px] text-[16px]'>It allows me to utilize my eye for aesthetics, attention to detail, and understanding of color harmony in a digital context. Just as a makeup artist enhances natural beauty, I now have the ability to transform digital landscapes and create visually stunning user experiences.</p>
     </div>
     <div className='flex justify-center items-center pt-[50px]'>
-    <img src={present} alt='present' className='ml-[100px] mr-[100px]'/>
+    <img src={present} alt='present' className='md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px]'/>
     </div>
-    <div className='ml-[100px] mr-[100px] pt-[50px]'>
-        <p className='text-[25px] pb-[30px]'>Future</p>
-        <p className='text-[20px]'>The world of frontend development offers endless opportunities for creative expression and innovation. It allows me to combine my love for aesthetics with technical skills to craft engaging and delightful experiences for users.
+    <div className='md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px] pt-[50px]'>
+        <p className='md:text-[25px] pb-[30px] text-[18px]'>Future</p>
+        <p className='md:text-[20px] text-[16px]'>The world of frontend development offers endless opportunities for creative expression and innovation. It allows me to combine my love for aesthetics with technical skills to craft engaging and delightful experiences for users.
 It's a dynamic field that constantly challenges me to learn and grow, and I couldn't be more excited about the possibilities it presents.</p>
     </div>
-    <div className='ml-[100px] mr-[100px] pt-[50px]'>
-        <p className='text-[20px] font-[600]'>Stay awesome!</p>
-        <p className='text-[20px] font-squarePeg'>Oyindamola Adegoke</p>
+    <div className='md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px] pt-[50px]'>
+        <p className='md:text-[20px] font-[600] text-[16px]'>Stay awesome!</p>
+        <p className='md:text-[20px] font-squarePeg text-[16px]'>Oyindamola Adegoke</p>
     </div>
     </>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,99 +13,99 @@ import portfolio from './assets/Portfolio.png';
 
 const Projects = () => {
   return (
-    <div className='mt-[50px] ml-[100px] mr-[100px]'>
-        <div className='flex gap-[2rem]'>
+    <div className='mt-[50px] md:ml-[100px] md:mr-[100px] ml-[20px] mr-[20px]'>
+        <div className='flex gap-[1.5rem]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 February 2024</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>TimeSync</p>
-                <p className='text-[20px]'>TimeSync is a web app that offers seemless experience in time tracking, ensuring precision and efficiency. Beyond that, TimeSync is your partner in effective project management, providing tools and features to enhance collaboration and productivity. </p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://time-sync-tau.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 February 2024</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>TimeSync</p>
+                <p className='md:text-[20px] text-[16px]'>TimeSync is a web app that offers seemless experience in time tracking, ensuring precision and efficiency. Beyond that, TimeSync is your partner in effective project management, providing tools and features to enhance collaboration and productivity. </p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://time-sync-tau.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={timesync} alt='timesync' className='rounded-[1rem]'/>
                 </div>
             </div>
         </div>
-        <div className='flex gap-[2rem] mt-[60px]'>
+        <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 July 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>Loanify</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 July 2023</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>Loanify</p>
+                <p className='md:text-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://project-loanify-app.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={loanify} alt='loanify' className='rounded-[1rem]'/>
                 </div>
             </div>
         </div>
-        <div className='flex gap-[2rem] mt-[60px]'>
+        <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 March 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>Todo App</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://codewithoyinda.github.io/Simple-Todo-App/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 March 2023</p>
+                <p className='md:text-[30px] text-[16px] font-[600] pb-[20px]'>Todo App</p>
+                <p className='md:-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://codewithoyinda.github.io/Simple-Todo-App/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={todo} alt='todo' className='rounded-[1rem]'/>
                 </div>
             </div>
         </div>
-        <div className='flex gap-[2rem] mt-[60px]'>
+        <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 15 April 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>Search Profile</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://company-profile-roan.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 15 April 2023</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>Search Profile</p>
+                <p className='md:text-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://company-profile-roan.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={profile} alt='profile' className='rounded-[1rem]'/>
                 </div>
             </div>
         </div>
-        <div className='flex gap-[2rem] mt-[60px]'>
+        <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>Xoxo</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://clothing-brand-wine.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 Oct 2023</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>Xoxo</p>
+                <p className='md:text-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://clothing-brand-wine.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={xoxo} alt='xoxo' className='rounded-[1rem]'/>
                 </div>
             </div>
         </div>
-        <div className='flex gap-[2rem] mt-[60px]'>
+        <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 May 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>Starwars Clone</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://star-wars-movie.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 May 2023</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>Starwars Clone</p>
+                <p className='md:text-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://star-wars-movie.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={starwars} alt='starwars' className='rounded-[1rem]'/>
                 </div>
             </div>
-        </div> <div className='flex gap-[2rem] mt-[60px]'>
+        </div> <div className='flex gap-[1.5rem] mt-[60px]'>
            <div>
-            <img src={avatar} alt='avatar' className='w-[350px] h-[150px] rounded-[50%]'/>
+            <img src={avatar} alt='avatar' className='md:w-[350px] md:h-[150px] w-[350px] h-[50px] rounded-[50%]'/>
             </div>
             <div>
-                <p className='text-[22px]'><span className='font-[600]'>Oyindamola .</span> 10 July 2023</p>
-                <p className='text-[30px] font-[600] pb-[30px]'>2023 Portfolio</p>
-                <p className='text-[20px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
-                <p className='pb-[30px] pt-[30px]'>Visit the site by clicking <Link to='https://oyindamola-a.vercel.app/' className='text-[#df5f83]'>here</Link></p>
+                <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span> 10 July 2023</p>
+                <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>2023 Portfolio</p>
+                <p className='md:text-[20px] text-[16px]'>Loanify is a web app that simplifies the management, monitoring and risk management, monitoring and risk assessment of all types of loans. In order to reduce loss incurred, Loanify assists loan officers and their financial institutions in ancticipating the likelihood of loan default.</p>
+                <p className='pb-[30px] pt-[30px] md:text-[16px] text-[12px]'>Visit the site by clicking <Link to='https://oyindamola-a.vercel.app/' className='text-[#df5f83]'>here</Link></p>
                 <div>
                     <img src={portfolio} alt='portfolio' className='rounded-[1rem]'/>
                 </div>

6a3d22a345ad74ed4735f819edf3c553deec8b1a
CodewithOyinda <honeybee08186@gmail.com>
2024-03-22 20:56:34 +0100

    see more and see less functionality implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,34 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='h-[100px]'>
-       <div className='flex justify-center items-center gap-[2rem] mt-[60px] mb-[20px]'>
-       <Link to='http://linkedin.com/in/oyindamola-adegoke'><IoLogoLinkedin /></Link>
-       <Link to='https://twitter.com/AdegokeOyindam9'><RiTwitterXFill /></Link>
-       <Link to='https://github.com/CodewithOyinda'><IoLogoGithub /></Link>
-       <Link to='https://instagram.com/o.y.i.n.d.a_?igshid=YmMyMTA2M2Y='><AiFillInstagram /></Link>
-       </div>
-       <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center gap-[2rem] mt-[60px] mb-[20px]'>
+        <span title="Connect with me on LinkedIn">
+          <Link to='http://linkedin.com/in/oyindamola-adegoke' className='hover:bg-[#e9e9e9] rounded-sm w-[25px] h-[25px] flex justify-center items-center'>
+            <IoLogoLinkedin />
+          </Link>
+        </span>
+        <span title="Follow me on Twitter">
+          <Link to='https://twitter.com/AdegokeOyindam9' className='hover:bg-[#e9e9e9] rounded-sm w-[25px] h-[25px] flex justify-center items-center'>
+            <RiTwitterXFill />
+          </Link>
+        </span>
+        <span title="My GitHub">
+          <Link to='https://github.com/CodewithOyinda' className='hover:bg-[#e9e9e9] rounded-sm w-[25px] h-[25px] flex justify-center items-center'>
+            <IoLogoGithub />
+          </Link>
+        </span>
+        <span title="Follow me on Instagram">
+          <Link to='https://instagram.com/o.y.i.n.d.a_?igshid=YmMyMTA2M2Y=' className='hover:bg-[#e9e9e9] rounded-sm w-[25px] h-[25px] flex justify-center items-center'>
+            <AiFillInstagram />
+          </Link>
+        </span>
+      </div>
+      <div className='flex justify-center items-center'>
         <p>Oyindamola © 2023</p>
-       </div>
+      </div>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
+
--- src/pages/Feeds.jsx
+++ src/pages/Feeds.jsx
@@ -4,8 +4,11 @@ import xplore from './assets/Xplore.png';
 import wtm from './assets/wtmakers.png';
 import fest from './assets/Devfest22.png';
 import avatar from './assets/Avatar.png';
+import { useCollapse } from 'react-collapsed'
 
 const Feeds = () => {
+
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
 
   const toggleFullScreen = (id) => {
       
@@ -36,8 +39,16 @@ const Feeds = () => {
                 <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>XPLORE 2023</p>
                 <p className='md:text-[20px] text-[16px]'>I had the privilege to attend a significant Tech Festival hosted by Lognetics Ltd at the NAF Conference Centre in Abuja. The event boasted an impressive lineup of speakers and diverse panel sessions covering topics such as Blockchain/Web3, Software Development, Start-ups, UI/UX, and various other fields in the tech industry. </p>
                 <p className='md:text-[20px] text-[16px]'>The sessions were incredibly engaging and provided valuable insights, particularly regarding Blockchain/Web3 technology. Panelists emphasized the wide-ranging applications of blockchain beyond cryptocurrency, highlighting its potential for solving diverse problems.</p>
+                   {isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See More</span>
+                   )}
+                <section {...getCollapseProps()}>
                 <p className='md:text-[20px] text-[16px]'>Additionally, discussions underscored the importance of understanding consumer mindset for successful blockchain adoption, shifting focus from solely manufacturer-centric perspectives. </p>
                 <p className='md:text-[20px] text-[16px]'>I also had the opportunity to gain knowledge from industry experts like Sam Harvard, who shared inspiring insights on innovation and its relevance in the tech sector. Overall, the event was enlightening, providing ample opportunities for networking, learning, and personal growth.</p>
+                </section>
+                { !isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See Less</span>
+                   )}
                 <div onClick={() => toggleFullScreen('xplore')} id='xplore'>
                     <img src={xplore} alt='xplore' className='rounded-[1rem] w-[649px] h-[400px] mt-[30px] object-cover'/>
                 </div>
@@ -56,11 +67,19 @@ const Feeds = () => {
                 <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>DevFest 2023</p>
                 <p className='md:text-[20px] text-[16px]'>We had a fantastic experience at the Devfest Abuja 2023, immersed in an empowering and lively community atmosphere. The event featured several speakers who delivered enlightening speeches. Here's a summary of key takeaways from the event:</p>
                 <p className='md:text-[20px] text-[16px]'>The journey is a marathon and not a sprint, just remain patient and focused.</p>
+                {isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See More</span>
+                   )}
+                <section {...getCollapseProps()}>
                 <p className='md:text-[20px] text-[16px]'>Done is better than perfect.</p>
                 <p className='md:text-[20px] text-[16px]'>The only thing worse than a bad decision is no decision.</p>
                 <p className='md:text-[20px] text-[16px]'>The best way to learn is teach.</p>
                 <p className='md:text-[20px] text-[16px]'>There's more than one path</p>
                 <p className='md:text-[20px] text-[16px]'>One day or Day one, you decide!</p>
+                </section>
+                { !isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See Less</span>
+                   )}
                 <div  onClick={() => toggleFullScreen('devfestone')} id='devfestone'>
                     <img src={devfest} alt='devfest' className='rounded-[1rem] w-[649px] h-[400px] mt-[30px]'/>
                 </div>
@@ -79,8 +98,16 @@ const Feeds = () => {
                 <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>Women Techmakers</p>
                 <p className='md:text-[20px] text-[16px]'>The International Women's Day event was hosted in Abuja by Women Techmakers. The speakers were exceptional, and here are some of the key tips I gleaned from the program:</p>
                 <p className='md:text-[20px] text-[16px]'>The Power of Intentionality.</p>
+                {isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See More</span>
+                   )}
+                <section {...getCollapseProps()}>
                 <p className='md:text-[20px] text-[16px]'>Your design super powers are the qualities that makes you stand out as a designer "Your unique selling point".</p>
                 <p className='md:text-[20px] text-[16px]'>Education is a powerful tool that enables women access numerous opportunities. </p>
+                </section>
+                { !isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See Less</span>
+                   )}
                 <div  onClick={() => toggleFullScreen('wtm')} id='wtm'>
                     <img src={wtm} alt='wtm' className='rounded-[1rem] w-[649px] h-[400px] mt-[30px] object-cover'/>
                 </div>
@@ -98,7 +125,15 @@ const Feeds = () => {
                 <p className='md:text-[22px] text-[16px]'><span className='font-[600]'>Oyindamola .</span>22 October 2022</p>
                 <p className='md:text-[30px] text-[18px] font-[600] pb-[20px]'>DevFest2022</p>
                 <p className='md:text-[20px] text-[16px]'> Participating in Devfest 2022 marked my introduction to the tech event scene. It provided an excellent platform for connecting with fellow developers. Among the many speakers, Ada Nduka Oyom, the founder of She Code Africa, shared a profound lesson that resonated with me: the significance of communities as drivers of growth in the tech field. </p>
+                {isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See More</span>
+                   )}
+                <section {...getCollapseProps()}>
                 <p className='md:text-[20px] text-[16px]'>She highlighted that communities consist of individuals with similar aspirations working towards common goals. This insight has profoundly influenced my career trajectory, serving as a driving force in my professional development.</p>
+                </section>
+                { !isExpanded ? '' : (
+                    <span {...getToggleProps()} className='md:text-[20px] text-[16px] text-[#fa2b66] hover:underline'>See Less</span>
+                   )}
                 <div  onClick={() => toggleFullScreen('devfesttwo')} id='devfesttwo'>
                     <img src={fest} alt='devfest' className='rounded-[1rem] w-[649px] h-[400px] mt-[30px] object-cover'/>
                 </div>
